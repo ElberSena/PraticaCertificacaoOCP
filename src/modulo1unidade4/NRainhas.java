d6f7c25abc254e1a4370dc6e8e25ae09cd67e8b5
ElberSena <elbersenna@hotmail.com>
2023-04-03 16:18:25 +0100

    Alterção exercicio 2

--- src/modulo1unidade4/NRainhas.java
+++ src/modulo1unidade4/NRainhas.java
@@ -1,5 +1,11 @@
 package modulo1unidade4;
 
+//Nesta atividade prática, deverá criar um programa que, ao receber um array
+//        de inteiros (independentemente do tamanho), o programa ordena o array de
+//        forma decrescente.
+//        Tem que implementar o algoritmo de ordenação, portanto não é permitido
+//        usar nenhum tipo de métodos do Java ou bibliotecas externas.
+
 import java.sql.Array;
 import java.util.Arrays;
 import java.util.Collections;
@@ -22,8 +28,5 @@ public class NRainhas {
 
         Arrays.sort(decrecente, Collections.reverseOrder());
         System.out.println("A ordem descecente do array: " + Arrays.toString(decrecente));
-
-
-
     }
 }
