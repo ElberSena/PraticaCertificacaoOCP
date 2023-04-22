6e4d84fb32e60d586328c759081adaa6b3eac724
ElberSena <elbersenna@hotmail.com>
2023-04-22 07:13:27 +0100

    Realização do exercício 3 parte 2

--- src/modulo1unidade4/Matrizes.java
+++ src/modulo1unidade4/Matrizes.java
@@ -1,5 +1,6 @@
 package modulo1unidade4;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 //1. Criar uma função estática double mediaArray(double[] nums) na classe Matrizes
@@ -20,13 +21,20 @@ public class Matrizes {
         System.out.println("Digite o tamanho do array");
         int tamanho = sc.nextInt();
 
-        double [] array = new double[tamanho];
-        for (int i = 0; i < array.length; i++){
+        double [] media = new double[tamanho];
+        for (int i = 0; i < media.length; i++){
             System.out.println("Digite os valores do array:");
-            array[i] = sc.nextDouble();
+            media[i] = sc.nextDouble();
         }
 
-        System.out.println("A média dos valores do array é " + mediaArray(array));
+        System.out.println("A média dos valores do array é " + mediaArray(media));
+
+        System.out.println("-------------------------------------------------------------");
+
+        int[] array = new int[]{17, 28, 9, 4, 22, 7, 1, 3, 19, 42, 58, 47};
+        System.out.println(Arrays.toString(array));
+
+        System.out.println("Os valores mínimos e máximos do array são: " + Arrays.toString(minMax(array)));
 
     }
     public static double mediaArray(double[] nums){
@@ -38,4 +46,23 @@ public class Matrizes {
         return media;
     }
 
+    public static int [] minMax (int[] nums){
+        int max = 0;
+        int min = 1;
+
+        for (int elemento : nums) {
+            if (elemento > max ){
+                max = elemento;
+            }
+            if (elemento < min){
+                min = elemento;
+
+            }
+
+        }
+        int [] array = new int [] {min,max};
+      return array;
+    }
+
 }
+
